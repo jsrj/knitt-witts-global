b07c584528cf4706122f401d344cbc037a4fc535
Arjay Jones <robert.jones@arjaycodes.com>
2019-03-06 00:35:35 -0800

    explicitly added django to the core installed apps; added inventory app to installed apps

--- knit_witts_global/settings.py
+++ knit_witts_global/settings.py
@@ -39,9 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django',
 
     # Project-Specific Apps
-    'inventory'
+    'inventory',
 ]
 
 MIDDLEWARE = [
