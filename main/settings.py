9510a0d4bc877386345bc7b362b77693983be5f3
Arjay Jones <robert.jones@arjaycodes.com>
2019-03-07 12:00:20 -0800

    added django heroku library

--- main/settings.py
+++ main/settings.py
@@ -132,3 +132,7 @@ if USE_TZ:
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
