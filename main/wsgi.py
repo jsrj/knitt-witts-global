93e5eed055231f7bc8eb0a7880c894f61311ed3e
Arjay Jones <robert.jones@arjaycodes.com>
2019-03-06 17:47:59 -0800

    refactored project structure such that the main app is named as such

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for knit_witts_global project.
+WSGI config for main project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "knit_witts_global.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 application = get_wsgi_application()
