690b00d647911f7465793dd7c4ca926e31df62eb
Arjay Jones <robert.jones@arjaycodes.com>
2019-03-07 21:30:37 -0800

    added temporary redirect to old domain of knitwittsbypaulette.com for production/release instance until new site is developed

--- core/views.py
+++ core/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 
 # Home Page - index.html
 class HomePageView(TemplateView):
 
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+        # return render(request, 'index.html', context=None)
+        return redirect('https://www.knitwittsbypaulette.com/')
 
     # def post(request, **kwargs):
     #     return render(request, '', context=None)
